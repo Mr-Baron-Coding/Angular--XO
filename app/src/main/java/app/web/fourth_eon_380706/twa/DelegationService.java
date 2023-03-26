eaed9c1ee7157986265340ef2dcf23a067f8dde3
BaronCoding <mikegbaron@gmail.com>
2023-03-26 08:14:37 +0300

    Update

---
+++ app/src/main/java/app/web/fourth_eon_380706/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.web.fourth_eon_380706.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
